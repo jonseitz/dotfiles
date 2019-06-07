04860c2af5ecb77cc42a26add6be79c6eaa2a051
Jonathan Seitz <jseitz@seas.harvard.edu>
2019-06-07 14:07:25 -0400

    fix output placement

--- .config/sway/displays/home.sh
+++ .config/sway/displays/home.sh
@@ -4,6 +4,6 @@ swaymsg output eDP-1 position 0 900
 swaymsg output "'Dell Inc. DELL U2412M YMYH14863FFL'" enable 
 swaymsg output "'Dell Inc. DELL U2412M YMYH14863FFL'" position 1920 0
 swaymsg output "'Dell Inc. DELL E2014H 12MWY4AN2FGU'" enable
-swaymsg output "'Dell Inc. DELL E2014H 12MWY4AN2FGU'" position 3520 0
+swaymsg output "'Dell Inc. DELL E2014H 12MWY4AN2FGU'" position 3840 0
 killall waybar
 exec waybar
