b1873c7d4a23de97709d22a94df92e5765df0370
Jonathan Seitz <jseitz@seas.harvard.edu>
2019-07-19 14:01:05 -0400

    sway updates

--- .config/sway/displays/work.sh
+++ .config/sway/displays/work.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-swaymsg output eDP-1 disable
+swaymsg output * disable
+swaymsg output eDP-1 enable 
+swaymsg output eDP-1 pos 5120 1000
 swaymsg output "'Dell Inc. DELL U2717D 67YGV74R780L'" enable
 swaymsg output "'Dell Inc. DELL U2717D 67YGV74R780L'" position 0 0
-swaymsg output "'Dell Inc. DELL U2717D 67YGV75QAAPS'"  pos 2560 0
 swaymsg output "'Dell Inc. DELL U2717D 67YGV75QAAPS'"  enable 
+swaymsg output "'Dell Inc. DELL U2717D 67YGV75QAAPS'"  position 2560 0
 killall waybar
 exec waybar
