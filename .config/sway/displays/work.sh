2bfb8967a1ec34fcd05427f04216d94e0d21f07d
Jonathan Seitz <jseitz@seas.harvard.edu>
2020-02-12 08:21:14 -0500

    rearrange monitors

--- .config/sway/displays/work.sh
+++ .config/sway/displays/work.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-swaymsg output * disable
+swaymsg output "*" disable
 swaymsg output eDP-1 enable 
-swaymsg output eDP-1 pos 5120 1000
+swaymsg output eDP-1 pos 0 0 res 1920x1080 scale 1
 swaymsg output "'Dell Inc. DELL U2717D 67YGV74R780L'" enable
-swaymsg output "'Dell Inc. DELL U2717D 67YGV74R780L'" position 0 0
-swaymsg output "'Dell Inc. DELL U2717D 67YGV75QAAPS'"  enable 
-swaymsg output "'Dell Inc. DELL U2717D 67YGV75QAAPS'"  position 2560 0
+swaymsg output "'Dell Inc. DELL U2717D 67YGV74R780L'" position 1920 0 res 2560x1440@60Hz scale 1
+swaymsg output "'Dell Inc. DELL U2717D 67YGV75QAAPS'" enable 
+swaymsg output "'Dell Inc. DELL U2717D 67YGV75QAAPS'" position 4480 0 res 2560x1440@60Hz scale 1
 killall waybar
 exec waybar
