dc3e587e782e3c1bb7501fbebdd552433b0b9e4c
Jonathan Seitz <mail@jonathanseitz.com>
2020-02-11 21:47:14 -0500

    update for new monitor

--- .config/sway/displays/home.sh
+++ .config/sway/displays/home.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
+swaymsg output "*" disable
 swaymsg output eDP-1 enable
-swaymsg output eDP-1 position 0 900 
-swaymsg output "'Dell Inc. DELL U2412M YMYH14863FFL'" enable 
-swaymsg output "'Dell Inc. DELL U2412M YMYH14863FFL'" position 1920 0
-swaymsg output "'Dell Inc. DELL E2014H 12MWY4AN2FGU'" enable
-swaymsg output "'Dell Inc. DELL E2014H 12MWY4AN2FGU'" position 3840 0
+swaymsg output eDP-1 position 2560 900
+swaymsg output "'Unknown HP Z27 CN49220GZG'" enable
+swaymsg output "'Unknown HP Z27 CN49220GZG'" scale 1.5
+swaymsg output "'Unknown HP Z27 CN49220GZG'" position 0 0
 killall waybar
 exec waybar
