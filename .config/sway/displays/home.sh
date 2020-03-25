cfeb84e60ff51d1fa3172a589839a09eb3f6c3e5
Jonathan Seitz <jseitz@seas.harvard.edu>
2020-03-25 08:15:08 -0400

    move laptop screen up

--- .config/sway/displays/home.sh
+++ .config/sway/displays/home.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 swaymsg output eDP-1 enable
-swaymsg output eDP-1 position 2560 900
+swaymsg output eDP-1 position 2560 250
 swaymsg output "'Unknown HP Z27 CN49220GZG'" enable
 swaymsg output "'Unknown HP Z27 CN49220GZG'" scale 1.5
 swaymsg output "'Unknown HP Z27 CN49220GZG'" position 0 0
