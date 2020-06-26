4627bfa8a797ef23ded90d675da9c3eb091a49ff
Jonathan Seitz <mail@jonathanseitz.com>
2020-06-25 22:16:46 -0400

    add notifications script

---
+++ .config/afew/notifications.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import gi
+gi.require_version('Notify', '0.7')
+from gi.repository import Notify
+from afew.filters.BaseFilter  import Filter
+from afew.FilterRegistry import register_filter
+
+
+@register_filter
+class Notifications(Filter):
+    message = 'Show notifications when new messages are received'
+    query = 'tag:inbox'
+    def handle_message(self, message):
+        Notify.init("New Mail")
+        sender = message.get_header('From')
+        subject = message.get_header('Subject')
+        NewMail = Notify.Notification.new('New Mail', 'From: {}\nSubj: {}'.format(sender,subject))
+        NewMail.show()
+        Notify.uninit()
